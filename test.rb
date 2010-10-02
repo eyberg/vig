b1a5d54e10d77393e9571a652ef92a382cc51b47
Ian Eyberg <ian@telematter.com>
2010-10-02 14:39:13 -0500

    adding in some README notes; classing out the example test; authenticate works but i dont think im pasing my header right so its not listing

--- test.rb
+++ test.rb
@@ -1,14 +1,55 @@
+require 'rubygems'
+require 'uri'
 require 'net/http'
 require 'net/https'
+require 'yaml'
 
-email = ""
-password = ""
+class GoogleConnect
+  attr_accessor :auth_token
 
-http = Net::HTTP.new('www.google.com', 443)
-http.use_ssl = true
-path = '/accounts/ClientLogin'
-headers = {'Content-Type'=> 'application/x-www-form-urlencoded'}
-blah = "accountType=HOSTED_OR_GOOGLE&Email=#{email}&Passwd=#{password}&service=cl&source=github-vig-0.1"
-resp, data = http.post(path, blah, headers)
+  def initialize
+    creds = YAML::load(File.open('creds.yml'))
 
-puts data 
+    user, pass = creds.collect{ |k,v| v }
+
+    authenticate(user, pass)
+  end
+
+  def authenticate(user, pass)
+    http = Net::HTTP.new('www.google.com', 443)
+    http.use_ssl = true
+    path = '/accounts/ClientLogin'
+    headers = {'Content-Type' => 'application/x-www-form-urlencoded'}
+    blah = "accountType=HOSTED_OR_GOOGLE&Email=#{user}&Passwd=#{pass}&service=cl&source=github-vig-0.1"
+    resp, data = http.post(path, blah, headers)
+
+    unless !resp.code.eql? 200
+      puts 'oh noes!'
+      exit
+    end
+
+    kvs = data.split
+
+    self.auth_token = kvs.last.split("=").last
+  end
+
+  def list_documents
+    url = URI.parse('https://docs.google.com/feeds/default/private/full')
+
+    req = Net::HTTP::Get.new(url.path)
+
+    puts self.auth_token
+    req.add_field("Authorization", "GoogleLogin auth=#{self.auth_token}")
+    req.add_field("GData-Version", "3.0")
+
+    http = Net::HTTP.new(url.host, url.port) #.start do |http|
+    http.use_ssl = true
+    res = http.request(req)
+
+    puts res.body
+  end
+
+end
+
+gc = GoogleConnect.new
+gc.list_documents
